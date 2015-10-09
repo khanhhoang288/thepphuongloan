d9d6d67ef41fe64a3c20e6e52f74e1a94cf629b3
khanhhoang288 <khanhhoang288@gmail.com>
2015-10-09 15:59:28 +0700

    doi logo page admin

--- thepphuongloan/wp-content/themes/thep/functions.php
+++ thepphuongloan/wp-content/themes/thep/functions.php
@@ -301,3 +301,17 @@ function search_exc_cats( $query ) {
   return $query;
 }
 ?>
+<?php 
+//doi link logo + logo page login
+// Thay doi duong dan logo admin
+function wpc_url_login(){
+return "http://thepphuongloan.com"; // duong dan vao website cua ban
+}
+add_filter('login_headerurl', 'wpc_url_login');
+
+// Thay doi logo admin wordpress
+function login_css() {
+wp_enqueue_style( 'login_css', get_template_directory_uri() . '/style_hoang.css' ); // duong dan den file css moi
+}
+add_action('login_head', 'login_css');
+?>
